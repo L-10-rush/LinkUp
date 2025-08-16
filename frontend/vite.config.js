183fcd50eeb3c9837c4cc281bf769cb49d718d50
leo <mondalaniket2005@gmail.com>
2025-08-16 09:40:48 +0530

    fixed the deployment issue2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,31 @@
-// frontend/vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			// Forward any request that starts with "/api" to your backend server
-			"/api": {
-				target: "http://localhost:5001",
-				changeOrigin: true, //  for CORS 
-			},
-		},
-	},
-});
+  plugins: [react()],
+})
+
+
+
+
+
+
+
+// // frontend/vite.config.js
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		proxy: {
+// 			// Forward any request that starts with "/api" to your backend server
+// 			"/api": {
+// 				target: "http://localhost:5001",
+// 				changeOrigin: true, //  for CORS 
+// 			},
+// 		},
+// 	},
+// });
